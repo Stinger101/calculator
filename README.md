1862acce764dac99b72b715bf90fa9af5c71662a
stinger101 <john.ngugi716@strathmore.edu>
2025-05-12 14:17:19 +0300

    Calculator and js class files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # calculator
+index.html contains the code for the calculator. The other files are related to js class.
---
+++ javascript_refresher.js
@@ -0,0 +1,37 @@
+// Variables
+let num1 = 10;
+var num2 = 45;
+
+// printing to console
+console.log("hello");
+
+//datatypes
+//strings
+let name = "John";
+let school = 'Strathmore';
+
+//integers
+let num3=4;
+
+//float
+let temperature = 23.8;
+
+//boolean
+let is_raining = false;
+let is_sunny = true;
+
+//operations
+let sum_of_both = num1+num2
+let diff_of_both = num1-num2
+let product_of_both = num1*num2
+let div_of_both = num1/num2
+
+console.log(sum_of_both)
+
+// functions
+function adder(first, second){
+    console.log(first+second)
+}
+
+//using with html
+document.getElementById()
